8b0764e830ec11dd2a218c8c95463461b12dd851
Ali Irsyaad Nursyaban <aliirsyaadnursyaban@gmail.com>
2021-08-24 17:27:28 +0700

    example grpc

---
+++ grpc/client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"context"
+
+	"github.com/aliirsyaadn/example-grpc/chat"
+	"google.golang.org/grpc"
+)
+
+func main(){
+	var conn *grpc.ClientConn
+
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+
+	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	message := chat.Message{
+		Body: "Hello from the client!",
+	}
+
+	res, err := c.SayHello(context.Background(), &message)
+
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+
+	log.Printf("Response from Server: %s", res.Body)
+}
--- grpc/server.go
+++ grpc/server.go
@@ -1,1 +1,27 @@
-package grpc
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/aliirsyaadn/example-grpc/chat"
+	"google.golang.org/grpc"
+)
+
+func main(){
+	lis, err := net.Listen("tcp", ":9000")
+
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	c := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &c)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+}
